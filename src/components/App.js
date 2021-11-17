e8e5d8b58983d445926f6ffd5067a3af9518312c
AJEET KUMAR <85954029+Aryandesire@users.noreply.github.com>
2021-11-17 11:01:08 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+class App extends Component {
+    constructor(props) {
+		super(props);
+	};
+
+    render() {
+    	return(
+    		<div id="main">
+				<p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy </p>
+    		</div>
+    	);
+    }
+}
+
+
+export default App;
+
